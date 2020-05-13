4f7bcee38a60311a51f650c07c967b7209896da9
Harry Flanagan <harryflanagan756@gmail.com>
2020-05-13 10:04:47 +0100

    Add project files.

---
+++ HarryFlanagan_s00189591/Phone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/***Author: Harry Flanagan***/
+/***Date: 13/05/202***/
+namespace HarryFlanagan_s00189591
+{
+    class Phone
+    {
+        /*Properties*/
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
+    }
+}
